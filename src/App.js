3b5aa9e04f2ef1aa28c5a758370ae827ccefec99
Krystal Guzman <kryguzman@gmail.com>
2020-02-19 09:43:06 -0800

    finished step 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -20,16 +22,15 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value= {{ products, addItem}}>
-				
-			<Navigation cart={cart} />
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-
+				<CartContext.Provider value={cart} >
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+			
+					<Route
+					path="/cart"
+					component={ShoppingCart} />
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
